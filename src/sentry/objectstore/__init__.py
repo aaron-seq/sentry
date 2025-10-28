fe6be4b554a179e3fa997538891b4bbc2c331c97
Noah Martin <noahm444@gmail.com>
2025-10-28 14:33:40 -0400

    Make endpoint work for snapshots too

--- src/sentry/objectstore/__init__.py
+++ src/sentry/objectstore/__init__.py
@@ -3,4 +3,4 @@ from sentry.objectstore.service import Client, ClientBuilder
 __all__ = ["attachments", "Client", "ClientBuilder"]
 
 attachments = ClientBuilder("attachments")
-app_icons = ClientBuilder("app-icons")
+preprod = ClientBuilder("preprod")
