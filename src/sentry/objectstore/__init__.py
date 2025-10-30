e6b41fa370188a1cfbbffd090701bc598236c938
Ryan Brooks <ryanjeffrey.brooks112@gmail.com>
2025-10-30 11:41:33 -0700

    init

--- src/sentry/objectstore/__init__.py
+++ src/sentry/objectstore/__init__.py
@@ -1,6 +1,6 @@
 from sentry.objectstore.service import Client, ClientBuilder
 
-__all__ = ["attachments", "Client", "ClientBuilder"]
+__all__ = ["attachments", "preprod", "Client", "ClientBuilder"]
 
 attachments = ClientBuilder("attachments")
 preprod = ClientBuilder("preprod")
